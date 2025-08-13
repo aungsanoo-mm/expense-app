1f92f203821202442b79d38ef56d27579da7aa18
aungsanoo1996 <aungsanoo2056@gmail.com>
2025-08-13 10:52:23 +0630

    aws-lb+twoEC2+multiAZDB Fine

--- update-feature+Postgresdb/main.tf
+++ update-feature+Postgresdb/main.tf
@@ -7,10 +7,11 @@ terraform {
   }
 }
 
+
 # --- RDS Security Group ---
 resource "aws_security_group" "rds_postgres" {
   name        = "rds-postgres-sg"
-  description = "Allow Postgres from EC2 only"
+  description = "Allow Postgres from EC2 only" 
 
   ingress {
     from_port       = 5432
@@ -18,14 +19,36 @@ resource "aws_security_group" "rds_postgres" {
     protocol        = "tcp"
     security_groups = [aws_security_group.expense_demo.id]
   }
-
+}
+resource "aws_security_group" "lb_security_group" {
+  name        = "lb-security-group"
+  description = "Allow HTTP and HTTPS traffic to the load balancer"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress  {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Optional Flask API
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
 }
+}
+
 
 # --- RDS Postgres Instance ---
 resource "aws_db_instance" "expense_rds" {
@@ -39,6 +62,7 @@ resource "aws_db_instance" "expense_rds" {
   parameter_group_name   = "default.postgres11"
   skip_final_snapshot    = true
   publicly_accessible    = false
+  multi_az               = true
   vpc_security_group_ids = [aws_security_group.rds_postgres.id]
 }
 
@@ -52,27 +76,63 @@ provider "aws" {
 }
 
 resource "aws_instance" "expense_app" {
+  count         = 2
   ami           = "ami-0b8607d2721c94a77" # Ubuntu 22.04
   instance_type = "t2.micro"
   key_name      = local.key_name
   tags = {
     Name = "Webapp"
   }
+  vpc_security_group_ids = [aws_security_group.expense_demo.id]
+
   provisioner "remote-exec" {
     inline = ["echo 'echo ssh for ready' "]
     connection {
       type        = "ssh"
       user        = local.ssh_user
       private_key = file(local.private_key_path)
-      host        = aws_instance.expense_app.public_ip
+      host        = self.public_ip
     }
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook -i '${aws_instance.expense_app.public_ip},' --private-key ${local.private_key_path} webserver.yaml -e 'postgres_host=${replace(aws_db_instance.expense_rds.endpoint, ":5432", "")} postgres_port=${var.db_port} postgres_user=${var.db_username} postgres_password=${var.db_password} postgres_db=${var.db_name}'"
+    command = "ansible-playbook -i '${self.public_ip},' --private-key ${local.private_key_path} webserver.yaml -e 'postgres_host=${replace(aws_db_instance.expense_rds.endpoint, ":5432", "")} postgres_port=${var.db_port} postgres_user=${var.db_username} postgres_password=${var.db_password} postgres_db=${var.db_name}'"
   }
+}
 
-  vpc_security_group_ids = [aws_security_group.expense_demo.id]
+resource "aws_lb" "expense_lb" {
+  name               = "expense-lb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = var.public_subnets
+  security_groups    = [aws_security_group.lb_security_group.id]
+}
+
+resource "aws_lb_target_group" "expense_tg" {
+  name     = "expense-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  health_check {
+    path = "/health"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "expense_tg_attachment" {
+  count            = 2
+  target_group_arn = aws_lb_target_group.expense_tg.arn
+  target_id        = aws_instance.expense_app[count.index].id
+  port             = 80
+}
+
+resource "aws_lb_listener" "expense_listener" {
+  load_balancer_arn = aws_lb.expense_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.expense_tg.arn
+  }
 }
 
 resource "aws_security_group" "expense_demo" {
--- update-feature+Postgresdb/variables.tf
+++ update-feature+Postgresdb/variables.tf
@@ -2,7 +2,8 @@ locals {
   ssh_user         = "ubuntu"
   private_key_path = "expenseapp.pem"
   key_name         = "expenseapp"
-
+  public_subnets   = ["subnet-0ef3c8767c2cc76a4"]
+  
 }
 variable "db_username" {
   default = "exp_user"
@@ -18,4 +19,15 @@ variable "db_name" {
 variable "db_port" {
   default = 5432
   
+}
+variable "vpc_id" {
+  description = "The VPC ID where resources will be deployed."
+  type        = string
+  default     = "vpc-0d3360c0d0d44198b"
+
+}
+variable "public_subnets" {
+  description = "List of public subnet IDs for the load balancer."
+  type        = list(string)
+  default     = ["subnet-0ef3c8767c2cc76a4","subnet-08dc7aa620486831d","subnet-061478a0a17988005"]
 }
